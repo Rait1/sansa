e937c478f7599d5b5009ce3c9f4dc135597f1aca
Rait Loos <rait.loos@gmail.com>
2012-12-15 15:21:39 +0200

    Added database. Implemented tweet model saving functionality.

--- application/classes/Controller/Dashboard.php
+++ application/classes/Controller/Dashboard.php
@@ -2,10 +2,15 @@
 
 class Controller_Dashboard extends Controller_Template
 {
+	public function action_index()
+	{
+		$new_tweet = $this->request->post('tweet');
+		if (strlen($new_tweet) > 0) {
+			$tweet_model = new Model_Tweet;
 
-    public function action_index()
-    {
-        $tweet = $this->request->post('tweet');
-    }
+			$tweet_model->add_new($new_tweet);
+			Notify::info(Notify::SUCCESS);
+		}
+	}
 
 } // End Welcome
