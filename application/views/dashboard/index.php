08ef75730156ae06651aacd356056f5543792282
Rait Loos <rait.loos@gmail.com>
2012-12-15 14:11:27 +0200

    Renamed welcome controller to dashboard. Added form for tweeting.

---
+++ application/views/dashboard/index.php
@@ -0,0 +1,7 @@
+<p class="lead">Tweet here</p>
+<form method="post" class="form-signin">
+    <textarea name="tweet" rows="3" placeholder="Type your tweet here"></textarea>
+    <br>
+    <button class="btn btn-success btn-primary" type="submit">Tweet</button>
+    </br>
+</form>
