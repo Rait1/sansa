8fd708530d09cc397b8f366f51f6b34934353419
Rait Loos <rait.loos@gmail.com>
2012-12-15 15:46:56 +0200

    Implemented list of tweets

--- application/classes/Controller/Dashboard.php
+++ application/classes/Controller/Dashboard.php
@@ -1,16 +1,21 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-
 class Controller_Dashboard extends Controller_Template
 {
 	public function action_index()
 	{
+
+
+		$tweet_model = new Model_Tweet;
 		$new_tweet = $this->request->post('tweet');
 		if (strlen($new_tweet) > 0) {
-			$tweet_model = new Model_Tweet;
+			$tweet_model->add_new($new_tweet);
+
 
 			$tweet_model->add_new($new_tweet);
 			Notify::info(Notify::SUCCESS);
 		}
+
+		$this->content->tweets = $tweet_model->get_tweets();
 	}
 
 } // End Welcome
--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,7 +1,26 @@
 <p class="lead">Tweet here</p>
 <form method="post" class="form-signin">
-    <textarea name="tweet" rows="3" placeholder="Type your tweet here"></textarea>
-    <br>
-    <button class="btn btn-success btn-primary" type="submit">Tweet</button>
-    </br>
+	<textarea name="tweet" rows="3" placeholder="Type your tweet here"></textarea><br>
+	<button class="btn btn-success btn-primary" type="submit">Tweet</button>
 </form>
+
+<h2>My Tweets</h2>
+
+<table class="table table-striped table-bordered">
+	<thead>
+	<tr>
+		<td>Time</td>
+		<th>Tweet</th>
+	</tr>
+	</thead>
+	<tbody>
+
+	<?php foreach ($tweets as $tweet): ?>
+	<tr>
+		<td><?php echo $tweet->created?></td>
+		<td><?php echo $tweet->content?></td>
+	</tr>
+		<?php endforeach?>
+
+	</tbody>
+</table>
